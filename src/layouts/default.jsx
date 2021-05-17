e61c43e58ce2db5ce2557f90d6db05d0ee84d92d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-05-17 15:11:28 +0800

    feat: designing welcome page (#7)

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -1,7 +1,7 @@
 
 export default function Layout({children}) {
   return (
-    <div className="flex flex-row justify-center items-center w-full h-screen mx-auto overflow-hidden bg-scheme-sky">
+    <div className="font-poppins flex flex-row justify-center items-center w-full h-screen mx-auto overflow-hidden bg-scheme-sky">
       <div className="flex w-full h-auto">
         {children}
       </div>
