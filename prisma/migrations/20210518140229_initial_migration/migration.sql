ca94bc4410108bc8a7958d9e9d8d636248eec61b
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-05-18 22:18:58 +0800

    feat: add new column in prisma schema

---
+++ prisma/migrations/20210518140229_initial_migration/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "description" TEXT,
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "address" TEXT,
+    "birthday" TEXT,
+    "anniversary" TEXT,
+    "relationship" TEXT,
+    "avatar_url" TEXT,
+    "slug" TEXT,
+    "facebook" TEXT,
+    "twitter" TEXT,
+    "instagram" TEXT,
+    "tiktok" TEXT,
+    "youtube" TEXT
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contact.phone_unique" ON "Contact"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contact.email_unique" ON "Contact"("email");
