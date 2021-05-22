d4a949265e2c4b7a00375d8176ec9a5b2e1c9059
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-05-22 16:23:00 +0800

    refactor: add api-request for update contact and fixing input-text setValue and defaultValues (#23)

--- src/pages/api/update-contact/[id].js
+++ src/pages/api/update-contact/[id].js
@@ -3,7 +3,15 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async(req, res) => {
+  const { id } = req.query
+  const getID = parseInt(id)
+  
   const data = JSON.parse(req.body)
-  const updatedContact = await prisma.contact.update({data})
+  const updatedContact = await prisma.contact.update({
+    where: {
+      id: getID
+    },
+    data
+  })
   res.json(updatedContact)
 }
