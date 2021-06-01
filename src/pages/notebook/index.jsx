6b5e02e8f1d845c4f8d9ee26da529577110e9b1b
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-01 10:41:54 +0800

    feat: create notes-list component (#49)
    * feat: create notes-list component
    
    * feat: import notes-list component

--- src/pages/notebook/index.jsx
+++ src/pages/notebook/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Layout from '~/layouts/default'
 import Sidebar from '~/components/sidebar'
-import SearchFunction from '~/components/search-function'
+import Noteslist from '~/components/notes-list'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
@@ -35,8 +35,8 @@ export default function Phonebook({countAllContacts}) {
             <div className="flex flex-row items-center w-full border-b border-scheme-mid px-3 py-5">
               <span className="font-bold text-xl text-scheme-dark">Notebook</span>
             </div>
-            <div className="flex flex-col w-full overflow-y-auto">
-              <h1>This is Notebook Page</h1>
+            <div className="grid grid-cols-3 gap-4 w-full h-full overflow-y-auto p-10">
+              <Noteslist />
             </div>
           </div>
         </div>
