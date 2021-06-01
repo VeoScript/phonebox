4b48250b2c88d32a92432465b6461e3a7cafb466
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-01 16:21:36 +0800

    style: designing notes slug page and notes list page (#62)

--- src/components/notes-list.jsx
+++ src/components/notes-list.jsx
@@ -8,7 +8,7 @@ export default function Notelist({ notes }) {
         return (
           <Link href={`/notebook/${ note.slug }`} key={ note.id }>
             <a className="flex flex-col w-full py-5">
-              <div className="flex flex-col h-[25rem] bg-scheme-pale text-[#333] rounded-xl transition ease-in-out duration-300 transform hover:scale-95">
+              <div className="flex flex-col h-[26rem] bg-scheme-pale text-[#333] rounded-xl transition ease-in-out duration-300 transform hover:scale-95">
                 <div className="relative">
                   <div className="absolute w-full inset-0 -top-5 z-30">
                     <div className="flex flex-row justify-center w-full">
@@ -23,7 +23,10 @@ export default function Notelist({ notes }) {
                         <span className="font-bold text-xl">{ note.title }</span>
                         <span className="font-light text-xs"><Moment date={ note.date } format='LLLL' /></span>
                       </div>
-                      <span className="font-normal text-sm line-clamp-3">{ note.note }</span>
+                      <div className="flex flex-col w-full space-y-3">
+                        <span className="font-normal text-sm line-clamp-3">{ note.note }</span>
+                        <span className="font-normal text-xs text-[#25C5F3]">Click to read more...</span>
+                      </div>
                       <div className="w-full max-w-xs pt-2">
                         <span className="font-normal text-xs bg-scheme-sky text-scheme-mid px-2 py-1 rounded-full">{ note.tag }</span>
                       </div>
