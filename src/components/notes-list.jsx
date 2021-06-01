798f6ef2c8544c205dd22a276e3f3d4af3eea567
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-01 13:09:41 +0800

    style: change card height

--- src/components/notes-list.jsx
+++ src/components/notes-list.jsx
@@ -7,7 +7,7 @@ export default function Notelist({ notes }) {
         return (
           <Link href={`/notebook/${ note.slug }`}>
             <a className="flex flex-col w-full py-5">
-              <div className="flex flex-col h-[23rem] bg-scheme-pale text-[#333] rounded-xl transition ease-in-out duration-300 transform hover:scale-95">
+              <div className="flex flex-col h-[25rem] bg-scheme-pale text-[#333] rounded-xl transition ease-in-out duration-300 transform hover:scale-95">
                 <div className="relative">
                   <div className="absolute w-full inset-0 -top-5 z-30">
                     <div className="flex flex-row justify-center w-full">
@@ -18,8 +18,10 @@ export default function Notelist({ notes }) {
                 <div className="relative">
                   <div className="absolute w-full inset-0 top-44 mt-5 z-10">
                     <div className="flex flex-col w-full space-y-2 px-3">
-                      <span className="font-bold text-xl">{ note.title }</span>
-                      <span className="font-light text-sm">{ note.date }</span>
+                      <div className="flex flex-col w-full">
+                        <span className="font-bold text-xl">{ note.title }</span>
+                        <span className="font-light text-sm">{ note.date }</span>
+                      </div>
                       <span className="font-normal text-sm line-clamp-3">{ note.note }</span>
                       <div className="w-full max-w-xs pt-5">
                         <span className="font-normal text-xs bg-scheme-sky text-scheme-mid px-2 py-1 rounded-full">{ note.tag }</span>
