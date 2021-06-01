7ce18f3e87f80295e7ff25bd241e1b0c556c692c
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-01 16:21:09 +0800

    style: designing notes slug page and notes list page

--- src/pages/notebook/[slug].jsx
+++ src/pages/notebook/[slug].jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Layout from '~/layouts/default'
+import Moment from 'react-moment'
 import Sidebar from '~/components/sidebar'
 import DeleteNotesModal from '~/components/notebook-modals/modal-delete-notes'
 import UpdateNotesModal from '~/components/notebook-modals/modal-update-notes'
@@ -33,6 +34,31 @@ export default function NotebookSlug({ note, countAllNotes, countAllContacts }) 
               <div className="flex flex-row items-center space-x-1">
                 <UpdateNotesModal getnote={ note } />
                 <DeleteNotesModal getnote={ note } />
+              </div>
+            </div>
+            <div className="flex flex-col w-full h-full overflow-y-auto">
+              <div className="relative">
+                <div className="absolute inset-0 w-full z-10">
+                  <img className="w-full h-80 bg-scheme-dark object-cover" src={ note.image } alt={ note.title } />
+                </div>
+              </div>
+              <div className="relative">
+                <div className="absolute w-full inset-0 top-[17rem] z-10">
+                  <div className="card flex flex-col w-full max-w-sm px-5 py-3 ml-10 rounded-xl bg-opacity-95 bg-scheme-dark text-scheme-pale">
+                    <span className="font-bold text-2xl">{ note.title }</span>
+                    <div className="flex flex-col w-full space-y-1">
+                      <span className="font-normal text-base">{ note.tag }</span>
+                      <span className="font-light text-xs"><Moment date={ note.date } format='LLLL' /></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="relative">
+                <div className="absolute w-full inset-0 top-[25rem] px-10 z-10">
+                  <div className="flex flex-col w-full">
+                    <div className="flex flex-col w-full px-10 py-5 mb-10 rounded-xl font-normal text-lg text-justify bg-opacity-95 bg-scheme-dark text-scheme-pale">{ note.note }</div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
