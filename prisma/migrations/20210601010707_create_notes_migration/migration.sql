7b2e769cb5b48f12e5c9d1850154363f15a6a3be
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-04 12:28:03 +0800

    feat: change SQLite to PostgreSQL (#80)

--- prisma/migrations/20210601010707_create_notes_migration/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Notes" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "image" TEXT,
-    "title" TEXT,
-    "slug" TEXT,
-    "note" TEXT,
-    "tag" TEXT
-);
